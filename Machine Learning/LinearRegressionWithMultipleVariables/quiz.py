3862e41ef3b49db37e3871b029c1422ec7aa4fa8
Zafar Takhirov <cc.rafaz@zafar.cc>
2015-05-02 15:25:16 -0700

    Finished the quiz

--- Machine Learning/LinearRegressionWithMultipleVariables/quiz.py
+++ Machine Learning/LinearRegressionWithMultipleVariables/quiz.py
@@ -5,11 +5,6 @@ import numpy as np
 
 """ Question 1"""
 class NormMatrix(object):
-    """
-    mean = np.array([])
-    minimum = np.array([])
-    maximum = np.array([])
-    """
     offset = np.array([])
     gain = np.array([])
 
@@ -22,64 +17,73 @@ class NormMatrix(object):
         return self.X[key]
         
     def _calcColumnVars(self):         # This thing gets the means and stuff
-        if self.X.shape != (0,):
+        if self.X.size != 0:
             self.X = self.X.astype(float)
             self.offset = self.X.mean(axis=0)
             gain = (self.X.max(axis=0) - self.X.min(axis=0))
             self.gain = np.where(gain==0, 1, gain)
 
-            """
-            self.mean = self.X.mean(axis=0)
-            self.minimum = self.X.min(axis=0)
-            self.maximum = self.X.max(axis=0)
-            """
-
     def normalize(self):       # Offset by mean and normalize to range
-        if ~self.normalized:
-            # normed = (self.X - self.mean) / (self.maximum - self.minimum)
+        if ~self.normalized & self.X.size != 0:
+            self._calcColumnVars()
             normed = (self.X - self.offset) / self.gain
             self.X = np.where(np.isnan(normed), self.X, normed)
             self.normalized = True
+            
+    def denormalize(self):
+        if self.normalized:
+            self.X = self.X*self.gain + self.offset
+            normalized = False
 
     def addColumn(self, column):
-        self.X = hstack(self.X, column)
+        column = np.array(column)[np.newaxis].transpose()
+        if column.size == 0:
+            return
+        # self.X = np.hstack((self.X, column))
+        if self.X.size == 0:
+            self.X = column
+        else:
+            self.X = np.hstack((self.X, column))
+        self._calcColumnVars()
+        self.normalized = False
+
+    def addRow(self, row):
+        row = np.array(row)
+        if row.size == 0:
+            return
+        # Adding rows denormalizes the results
+        if self.X.size == 0:
+            self.__init__(row);
+            # self.X = np.hstack((self.X, np.array(row)))
+        else:
+            if self.normalized:
+                self.denormalize()
+            self.X = np.vstack((self.X, np.array(row)))
+            # self.show()    
+        # self.normalize()
         self.normalized = False
         
     def show(self):
-        print "X = %s"%np.array_str(self.X, precision=2)
-        print "Mean = %s"%np.array_str(self.mean, precision=2)
-        print "Min = %s"%np.array_str(self.minimum, precision=2)
-        print "Max = %s"%np.array_str(self.maximum, precision=2)
-        print "Normalized: %s"%self.normalized
+        print "------------------------------------------------"
+        print "X = %s" % np.array_str(self.X, precision=2)
+        print "Gain = %s" % np.array_str(self.gain, precision=2)
+        print "Offset = %s" % np.array_str(self.offset, precision=2)
+        print "Normalized: %s" % self.normalized
     
 def q1():
     print 'Question 1'
-    x = NormMatrix([1])
-    x.show()
-    # x.addColumn(
-    """
-    x = np.array([
-        [89,72,94,69]]
-    )
-    x = np.append(x, x**2, axis=0)
-    x = x.transpose()
-
-    y = np.array([96, 44, 87, 78])
-
-    x,_ = normalize(x)
-    # print x
-    _.cout()
-    """
-
+    x = NormMatrix()
+    ar = np.array([89,72,94,69])
+    x.addColumn(ar)
+    x.addColumn(ar**2)
+    x. normalize()
+    print x[1,1]
+    print x[2,0]
+    
 def quiz():
     print '------------------------------------------------'
     q1()
 
 if __name__ == '__main__':
     quiz()
-    """
-    X = NormMatrix(np.array([[1,1,1,1], [89,72,94,69], [1,2,3,4]]).transpose())
-    X.show()
-    X.normalize()
-    X.show()
-    """
+
