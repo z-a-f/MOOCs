5ba6ceb12c3147a8d67b0939591ed3656a2013b8
Zafar Takhirov <cc.rafaz@zafar.cc>
2016-10-06 17:29:26 -0700

    Finished week1 -- all tests passed

--- Coursera/RoR/Week1/fullstack-course1-module2/Assignments/Lesson03-Assignment01-Classes/student-start/module2_lesson3_formative.rb
+++ Coursera/RoR/Week1/fullstack-course1-module2/Assignments/Lesson03-Assignment01-Classes/student-start/module2_lesson3_formative.rb
@@ -1,6 +1,6 @@
 class Person
   #have a first_name and last_name attribute with public accessors
-  #attr_accessor 
+  attr_accessor :first_name, :last_name
 
   #have a class attribute called `people` that holds an array of objects
   @@people = []
@@ -9,6 +9,9 @@ class Person
   def initialize(x,y)#should take 2 parameters for first_name and last_name
     #assign those parameters to instance variables
     #add the created instance (self) to people class variable
+    @first_name = x
+    @last_name = y
+    @@people.push(self)
   end
 
   #have a `search` method to locate all people with a matching `last_name`
@@ -16,11 +19,13 @@ class Person
     #accept a `last_name` parameter
     #search the `people` class attribute for instances with the same `last_name`
     #return a collection of matching instances
+    @@people.select { |inst| inst.last_name == last_name }
   end
 
   #have a `to_s` method to return a formatted string of the person's name
   def to_s
     #return a formatted string as `first_name(space)last_name`
+    "#{@first_name} #{@last_name}"
   end
 end
 
