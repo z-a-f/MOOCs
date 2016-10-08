f7c2d4ec5ae4c0568555a0780c1f7b20d901ac71
Zafar Takhirov <cc.rafaz@zafar.cc>
2016-10-08 03:46:08 -0700

    Finished the final

--- Coursera/RoR/Week3/first_app/app/controllers/courses_controller.rb
+++ Coursera/RoR/Week3/first_app/app/controllers/courses_controller.rb
@@ -1,6 +1,6 @@
 class CoursesController < ApplicationController
   def index
-  	@search_term = params[:looking_for] || 'stanford'
+  	@search_term = params[:looking_for] || 'jhu'
   	@courses = Coursera.for(@search_term)
   	@courses ||= []
   end
--- Coursera/RoR/Week3/fullstack-course1-module3/Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
+++ Coursera/RoR/Week3/fullstack-course1-module3/Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
@@ -1,5 +1,15 @@
 require 'httparty'
-
+require 'pp'
+	
 class Recipe
+	include HTTParty
 
+	base_uri		'http://food2fork.com/api'
+	default_params	key: ENV['FOOD2FORK_KEY']
+	format 			:json
+
+	def self.for term
+		#pp ENV
+		get("/search", query: {q: term})['recipes']
+	end
 end
